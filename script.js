f9f43a9065950cabf412c4381832a1d0e436a0bd
pndbear <37593571+pndbear@users.noreply.github.com>
2019-10-24 17:25:07 +0200

    jQuery scrollTop functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 console.log('jhashhah');
 
+$(document).ready(function() {});
+
+$('.js--scroll-to-maestros').click(function() {
+   $('html, body').animate({ scrollTop: $('.js--section-maestros').offset().top }, 1000);
+});
+$('.js--scroll-to-program').click(function() {
+   $('html, body').animate({ scrollTop: $('.js--section-program').offset().top }, 1500);
+});
+
 document.querySelector('.logo');
