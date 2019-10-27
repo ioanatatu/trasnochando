22a8b6abf206f72a29f3e94c37031ca3ad03ec52
pndbear <37593571+pndbear@users.noreply.github.com>
2019-10-27 14:41:00 +0100

    added sticky navbar and scroll to top button

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 console.log('jhashhah');
 
-$(document).ready(function() {});
+const btnScrollToTop = document.getElementById('ScrollToTop');
 
-$('.js--scroll-to-maestros').click(function() {
-   $('html, body').animate({ scrollTop: $('.js--section-maestros').offset().top }, 1000);
-});
-$('.js--scroll-to-program').click(function() {
-   $('html, body').animate({ scrollTop: $('.js--section-program').offset().top }, 1500);
+btnScrollToTop.addEventListener('click', () => {
+   window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+   });
 });
 
-document.querySelector('.logo');
+$(document).ready(function() {
+   $('.js--scroll-to-maestros').click(function() {
+      $('html, body').animate(
+         { scrollTop: $('.js--section-maestros').offset().top },
+         1000
+      );
+   });
+   $('.js--scroll-to-program').click(function() {
+      $('html, body').animate(
+         { scrollTop: $('.js--section-program').offset().top },
+         1500
+      );
+   });
+
+   document.querySelector('.logo');
+   $('.js--section-highlight').waypoint(
+      function(direction) {
+         if (direction == 'down') {
+            $('nav').addClass('sticky');
+         } else {
+            $('nav').removeClass('sticky');
+         }
+      },
+      { offset: '70px' }
+   );
+});
+
+/* var waypoint = new Waypoint({
+  element: document.getElementById('element-waypoint'),
+  handler: function(direction) {
+    notify(this.element.id + ' triggers at ' + this.triggerPoint)
+  },
+  offset: '75%'
+})*/
