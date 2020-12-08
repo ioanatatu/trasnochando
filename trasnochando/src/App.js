b569f28951c825dcf4b4195e3fcd5137c037cca1
ioanatatu <tatu.ioana@gmail.com>
2020-12-08 15:20:24 +0100

    Create React App

---
+++ trasnochando/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">some text here</header>
+        </div>
+    );
+}
+
+export default App;
